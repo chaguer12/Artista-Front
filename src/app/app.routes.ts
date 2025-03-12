a7bd23ba01fca602d31c025af0f5ebb58927c779
zoolotoui <chag.med.amine@gmail.com>
2025-03-12 21:14:03 +0000

    stat: working on studio owner page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ClientFormComponent } from './components/forms/client-form/client-form.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { OwnerComponent } from './components/owner/owner.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path:'contact',component:ContactComponent},
     {path:'register',component:ClientFormComponent},
-    {path:'about-us',component:AboutUsComponent}
+    {path:'about-us',component:AboutUsComponent},
+    {path: 'owner',component:OwnerComponent}
 ];
