636a18fbab7790c77798cfa83d261d9a6b14379c
zoolotoui <chag.med.amine@gmail.com>
2025-03-16 02:04:37 +0000

    stat: fixed cors and auth error

--- src/app/services/provider.service.ts
+++ src/app/services/provider.service.ts
@@ -1,33 +1,38 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../models/user';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 @Injectable({
   providedIn: 'root'
 })
 export class ProviderService {
 
-  private apiUrl = "http://localhost:8082/provider"
-  constructor(private http:HttpClient) { }
-
+      private apiUrl = "http://localhost:8082/provider"
+      constructor(private http:HttpClient,private authService:AuthService) { }
   
-    
-    getUsers():Observable<User[]>{
-        return this.http.get<User[]>(this.apiUrl + "/all")
+  
+      private getHeaders() {
+        const token = this.authService.getToken();
+        const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+        return headers;
+      }
+      getUsers():Observable<User[]>{
+        return this.http.get<User[]>(this.apiUrl + "/get", { headers: this.getHeaders() })
       }
     
       getUserById(id:number):Observable<User>{
-        return this.http.get<User>(`${this.apiUrl}/${id}`);
+        return this.http.get<User>(`${this.apiUrl}/${id}`,{headers:this.getHeaders()});
       }
     
       createUser(user:User):Observable<User>{
-        return this.http.post<User>(this.apiUrl,user);
+        return this.http.post<User>(this.apiUrl,user,{headers:this.getHeaders()});
       }
       updateUser(id: number, user: User): Observable<User> {
-        return this.http.patch<User>(`${this.apiUrl}/${id}`, user);
+        return this.http.patch<User>(`${this.apiUrl}/${id}`, user,{headers:this.getHeaders()});
       }
     
       deleteUser(id: number): Observable<void> {
-        return this.http.delete<void>(`${this.apiUrl}/${id}`);
+        return this.http.delete<void>(`${this.apiUrl}/${id}`,{headers:this.getHeaders()});
       }
 }
